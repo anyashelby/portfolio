3ca8bcf958c901e5b66bdce9ade568b2e37abed9
Andrey Holovanov <andreyhlvn@gmail.com>
2023-02-04 20:58:21 +0100

    Second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
+const list = document.querySelector('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+list.addEventListener('click', () => {
     menu.classList.remove('active');
 });
