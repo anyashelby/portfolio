06d282faa3c9f148afab6e82531cccfaaf8f6372
Andrey Holovanov <andreyhlvn@gmail.com>
2023-02-08 20:10:29 +0100

    Last section

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
 const closeList = document.querySelector('.menu__list');
+const overlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -19,6 +20,16 @@ document.addEventListener('keydown', function(e){
     if( e.keyCode == 27 ){
         menu.classList.remove('active');    
     }; 
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove("active");
+});
+
+const counter = document.querySelectorAll('.skills__rating-counter'),
+      lines = document.querySelectorAll('.skills__rating-line span');
+counter.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
 
 new WOW().init();
